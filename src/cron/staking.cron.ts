fba2af9906171d3ffeb3ddb3713082abaf7b206b
goran9999 <gutovicgoran@gmail.com>
2024-04-24 10:46:03 +0200

    fix

--- src/cron/staking.cron.ts
+++ src/cron/staking.cron.ts
@@ -15,13 +15,13 @@ export const stakingCron = new CronJob("*/10 * * * *", async () => {
 
     const wallets = (await User.find().select("wallet")).map((u) => u.wallet);
 
-    const stakedWallets = new Set();
+    const stakedWallets: { wallet: string; stakedAmount: number }[] = [];
 
     const staked = await Promise.all(
       wallets.map(async (w) => {
         const stakedNfts = await gamubllsContract.getStakedNfts(w);
         const len = stakedNfts.filter((s: any) => s[2]).length;
-        if (len > 0) stakedWallets.add({ wallet: w, stakedNfts: len });
+        if (len > 0) stakedWallets.push({ stakedAmount: len, wallet: w });
 
         return len;
       })
@@ -29,12 +29,10 @@ export const stakingCron = new CronJob("*/10 * * * *", async () => {
 
     const totalStaked = staked.reduce((acc, val) => acc + val, 0);
 
-    const w = Array.from(wallets).map((w) => w);
-
     logger.info(`Cron found ${totalStaked} staked nfts!`);
 
     await client.set("stakedNfts", totalStaked.toString());
-    await client.set("wallets", JSON.stringify(w));
+    await client.set("wallets", JSON.stringify(stakedWallets));
 
     logger.info("Stored staking info!");
 
